5f95eedbd62ae5928b5959b3f891aaf8bdb54146
e.korolev <e.korolev@thecoders.ru>
2022-04-07 22:17:47 +0500

    Start (07.04)

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type {NextPage} from 'next'
+import style from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+    return (
+        <div className={style.content}>123</div>
+    )
+}
+
+export default Home
